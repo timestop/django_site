b92f4875d2c07a83f45352b7b889d4eaf4f674d1
Charlie Hung <timestop.tw@gmail.com>
2017-01-23 02:53:40 +0800

    Add resturant data and overwrite __unicode__ method in models.py

--- resturants/models.py
+++ resturants/models.py
@@ -9,9 +9,15 @@ class Resturant(models.Model):
     phone_number = models.CharField(max_length=15)
     address = models.CharField(max_length=50, blank=True)
 
+    def __unicode__(self):
+        return self.name
+
 class Food(models.Model):
     name = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=3, decimal_places=0)
     comment = models.CharField(max_length=50, blank=True)
     is_spicy = models.BooleanField(default=False)
     resturant = models.ForeignKey(Resturant)
+
+    def __unicode__(self):
+        return self.name
